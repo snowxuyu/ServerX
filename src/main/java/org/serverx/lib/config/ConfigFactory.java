fff0515df9edc7d014d6bacffa513cdba5b41f50
minghu.zhang <minghu.zhang@icoud.com>
2017-07-18 10:40:57 +0800

    解析输出

--- src/main/java/org/serverx/lib/config/ConfigFactory.java
+++ src/main/java/org/serverx/lib/config/ConfigFactory.java
@@ -1,5 +1,10 @@
 package org.serverx.lib.config;
 
+/**
+ * 配置导出接口
+ * @author minghu
+ *
+ */
 public class ConfigFactory {
 
 }
--- src/main/java/org/serverx/lib/netty/DataFlow.java
+++ src/main/java/org/serverx/lib/netty/DataFlow.java
@@ -1,5 +1,11 @@
 package org.serverx.lib.netty;
 
+/**
+ * 参数解析
+ * @author minghu
+ *
+ * @param <T>
+ */
 public abstract class DataFlow<T> {
 
 }
